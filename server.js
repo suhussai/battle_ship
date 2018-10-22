e1ebeb694bef911f54d807e9f27532c6ee5557fc
suhussai <suhussai@ualberta.ca>
2018-10-21 20:00:46 -0600

    package as nodejs app

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
